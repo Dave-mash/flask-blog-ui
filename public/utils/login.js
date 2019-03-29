e267b6076deb3f35bf84e1751b6dd4f278b68bd6
Dave-mash <macharia3041@gmail.com>
2019-03-29 22:06:31 +0300

    deploy heroku

--- public/utils/login.js
+++ public/utils/login.js
@@ -1,8 +1,10 @@
 // user log in event
 
-let span = document.getElementById('error_span');
-const form = document.getElementById('login-form');
-const loginUrl = window.location.hostname === 'localhost' ? 'http://127.0.0.1:3000/auth/login' : 'https://flask-blogify.herokuapp.com/auth/login';
+const form = document.getElementById('login-form')
+const loginUrl = 'http://127.0.0.1:5000/api/v1/auth/login'; // 'https://flask-blog-api.herokuapp.com/api/v1/auth/login';
+const URL = window.location.hostname === 'localhost' ? 'http://127.0.0.1:3000' : 'https://flask-blogify.herokuapp.com';
+
+let span = document.getElementById('error_span')
 
 
 form.addEventListener('submit', (e) => {
@@ -26,58 +28,64 @@ form.addEventListener('submit', (e) => {
     // highlights empty fields
 
     fetch(loginUrl, {
-        mode: 'cors',
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Credentials': true
-        }
-    }).then(response => {
-        return response.json();
-    }, networkError => console.log(networkError.message)).then(jsonResponse => {
-        console.log(jsonResponse)
-        span.innerHTML = '';
-        let emailInput = document.getElementById(`email`)
-        let passwordInput = document.getElementById(`password`)
-
-        if (!email) {
-            let b = `<b class="error_message" style="color:red;">Email is required!</b>`
-            span.innerHTML = b
-            emailInput.style.border = '1.8px solid red'
-        } else if (!password) {
-            let b = `<b class="error_message" style="color:red;">Password is required!</b>`
-            span.innerHTML = b
-            passwordInput.style.border = '1.8px solid red'
-        } else {
+            mode: 'cors',
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': true
+            }
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else if (response.status === 404) {
+                let b = `<b class="error_message" style="color:red;">Details not found! Try loggin in.</b>`
+                span.innerHTML = '';
+                span.innerHTML = b;
+            }
+        }, networkError => console.log(networkError.message))
+        .then(jsonResponse => {
             span.innerHTML = '';
-            passwordInput.style.border = 'none'
-            emailInput.style.border = 'none'
-        }
+            let emailInput = document.getElementById(`email`)
+            let passwordInput = document.getElementById(`password`)
 
-        if (span.children.length == 0) {
-            console.log(jsonResponse)
-            if (jsonResponse.error) {
-                let b = `<b class="error_message" style="color:red;">${jsonResponse.error}</b>`
-                document.getElementById('error_span').innerHTML = b
+            if (!email) {
+                let b = `<b class="error_message" style="color:red;">Email is required!</b>`
+                span.innerHTML = b
+                emailInput.style.border = '1.8px solid red'
+            } else if (!password) {
+                let b = `<b class="error_message" style="color:red;">Password is required!</b>`
+                span.innerHTML = b
+                passwordInput.style.border = '1.8px solid red'
             } else {
-                document.getElementById('error_span').innerHTML = ''
+                span.innerHTML = '';
+                passwordInput.style.border = 'none'
+                emailInput.style.border = 'none'
             }
-            console.log(jsonResponse.error)
-        }
-        if (!jsonResponse.error) {
-            let details = {
-                id: jsonResponse.id,
-                auth_token: jsonResponse.auth_token,
-                username: jsonResponse.username
+
+            if (span.children.length == 0) {
+                console.log(jsonResponse)
+                if (jsonResponse.error) {
+                    let b = `<b class="error_message" style="color:red;">${jsonResponse.error}</b>`
+                    document.getElementById('error_span').innerHTML = b
+                } else {
+                    document.getElementById('error_span').innerHTML = ''
+                }
+                console.log(jsonResponse.error)
             }
-            setCookie(jsonResponse.username, JSON.stringify(details), 1);
-            if (!document.referrer.includes('username')) {
-                window.location.replace('http://127.0.0.1:3000/index.html?username=' + jsonResponse.username);
-            } else {
-                window.location.href = document.referrer;
-                console.log(window.location.href);
+            if (!jsonResponse.error) {
+                let details = {
+                    id: jsonResponse.id,
+                    auth_token: jsonResponse.auth_token,
+                    username: jsonResponse.username
+                }
+                setCookie(jsonResponse.username, JSON.stringify(details), 1);
+                if (!document.referrer.includes('username')) {
+                    window.location.replace(`${URL}/index.html?username` + jsonResponse.username);
+                } else {
+                    window.location.href = document.referrer;
+                    console.log(window.location.href);
+                }
             }
-        }
-    });
+        });
 });
